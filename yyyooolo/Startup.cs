43692fa158ad9458da298a369b03bb7a6e95df6c
urbx <47186753+urbx@users.noreply.github.com>
2019-06-05 19:49:52 +0200

    Dodaj pliki projektów.

---
+++ yyyooolo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(yyyooolo.Startup))]
+namespace yyyooolo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
